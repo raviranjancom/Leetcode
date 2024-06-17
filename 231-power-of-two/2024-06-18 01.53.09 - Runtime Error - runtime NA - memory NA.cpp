b74f37143f02acaeff32c4d1d9f43e316ab08b2c
raviranjancom <erraviranjan030198@gmail.com>
2024-06-18 01:53:09 +0530

    Add 231-power-of-two: Runtime Error - runtime NA - memory NA

---
+++ 231-power-of-two/2024-06-18 01.53.09 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,20 @@
+int fun(int n){
+    if (n==2) return 2;
+    if (n==1) return 1;
+    else{
+        return 2*fun(n-1);
+    }
+}
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int flag=0;
+        for(int i=1;i<=n;i++){
+            if (fun(i)==n){
+                flag++;
+            }
+        }
+        if (flag>0) return true;
+        else return false;
+    }
+};
