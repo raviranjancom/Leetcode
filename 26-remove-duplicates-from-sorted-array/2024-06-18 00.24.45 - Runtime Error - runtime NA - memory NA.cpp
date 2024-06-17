af43174ca67fd801872f4a0db718d69bc203c234
raviranjancom <erraviranjan030198@gmail.com>
2024-06-18 00:24:45 +0530

    Add 26-remove-duplicates-from-sorted-array: Runtime Error - runtime NA - memory NA

---
+++ 26-remove-duplicates-from-sorted-array/2024-06-18 00.24.45 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        vector<int> v;
+        int j=0;
+        if (nums.size()>1){
+            for(int i=1;i<=nums.size();i++){
+            if (nums[i-1]!=nums[i]){
+                v.push_back(nums[i-1]);
+                j++;
+            }
+        }
+        }
+        else{
+            v.push_back(nums[0]);
+            j=1;
+        }
+        for (int i=0;i<j;i++){
+            nums[i]=v[i];
+        }
+        return j;
+    }
+};
