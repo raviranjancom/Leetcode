1aeca57f835d5a89687c0f05a726f2384d9455a6
raviranjancom <erraviranjan030198@gmail.com>
2024-06-18 01:28:04 +0530

    Add 412-fizz-buzz: Wrong Answer - runtime NA - memory NA

---
+++ 412-fizz-buzz/2024-06-18 01.28.04 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> answer;
+        string y;
+        for (int i=1;i<n;i++){
+            if (i%15==0){
+                y="FizzBuzz";
+                answer.push_back(y);
+            }
+            else if(i%3==0){
+                y="Fizz";
+                answer.push_back(y);
+            }
+            else if(i%5==0){
+                y="Buzz";
+                answer.push_back(y);
+            }
+            else{
+                answer.push_back(to_string(i));
+            }
+        }
+        return answer;
+    }
+};
