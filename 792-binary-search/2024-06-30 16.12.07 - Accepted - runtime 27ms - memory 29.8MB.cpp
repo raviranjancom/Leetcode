3c399824f3030efa4205033f4ace6a959b7746b7
raviranjancom <erraviranjan030198@gmail.com>
2024-06-30 16:12:07 +0530

    Add 792-binary-search: Accepted - runtime 27ms - memory 29.8MB

---
+++ 792-binary-search/2024-06-30 16.12.07 - Accepted - runtime 27ms - memory 29.8MB.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int a=0,b=n-1,mid=(a+b)/2;
+        while(a<=b){
+            mid=(a+b)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>target) b=mid-1;
+            else a=mid+1;
+        }
+        return -1;
+    }
+};
