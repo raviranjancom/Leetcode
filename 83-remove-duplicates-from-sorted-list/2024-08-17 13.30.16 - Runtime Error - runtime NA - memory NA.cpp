ae8592c9d26439b32a7c0301f439254aac747d76
raviranjancom <erraviranjan030198@gmail.com>
2024-08-17 13:30:16 +0530

    Add 83-remove-duplicates-from-sorted-list: Runtime Error - runtime NA - memory NA

---
+++ 83-remove-duplicates-from-sorted-list/2024-08-17 13.30.16 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *p=head,*t=head;
+        p->next?p=p->next:NULL;
+        while(p){
+            if(t->val==p->val){
+                t->next=p->next;
+                p->next=NULL;
+                p=t->next;
+            }
+            else{
+                t=t->next;
+                t->next?p=p->next:NULL;
+            }
+        }
+        return head;
+    }
+};
