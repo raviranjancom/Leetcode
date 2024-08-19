a4d91cdc8fc5c8060aad31112b6af6bc4581b578
raviranjancom <erraviranjan030198@gmail.com>
2024-08-19 21:02:30 +0530

    Add 20-valid-parentheses: Runtime Error - runtime NA - memory NA

---
+++ 20-valid-parentheses/2024-08-19 21.02.30 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+class stackf{
+    public:
+    int size;
+    int top;
+    char *p;
+    stackf(int size,int top,char *p){
+        this->size=size;
+        this->top=top;
+        this->p=p;
+    }
+    void pushf(char v){
+        top++;
+        p[top]=v;
+    }
+    void popf(){
+        top--;
+    }
+    bool isemptyf(){
+        if(top==-1) return true;
+        else return false;
+    }
+    
+    char topf(){
+        return p[top];
+    }
+};
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stackf a(sizeof(s), -1 ,new char[10001]);
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i]=='(' || s[i]=='{' || s[i]=='[') a.pushf(s[i]);
+        else if((s[i]==')' && a.topf()=='(') || (s[i]=='}' && a.topf()=='{') || (s[i]==']' && a.topf()=='[')) a.popf();
+        else return false;
+        }
+        if(a.isemptyf()==true) return true;
+        else return false;
+    }
+};
