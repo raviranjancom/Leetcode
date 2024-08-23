71337746aae5b236334cfdf8f37d72d4b3f22311
raviranjancom <erraviranjan030198@gmail.com>
2024-08-23 21:55:25 +0530

    Add 3463-alternating-groups-i: Accepted - runtime 4ms - memory 26.4MB

---
+++ 3463-alternating-groups-i/2024-08-23 21.55.25 - Accepted - runtime 4ms - memory 26.4MB.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors) {
+        int n=size(colors),c=0;
+        for(int i=1;i<n-1;i++){
+            if(colors[i] != colors[i-1] && colors[i] != colors[i+1]){
+                c++;
+            }
+        }
+        if(colors[n-1] != colors[0] && colors[0] != colors[1]) c++;
+        if(colors[n-1] != colors[n-2] && colors[n-1] != colors[0]) c++;
+        return c;
+    }
+};
