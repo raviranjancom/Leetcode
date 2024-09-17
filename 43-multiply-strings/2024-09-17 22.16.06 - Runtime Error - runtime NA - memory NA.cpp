17082720df4469cce2088567597c0c6335bb3140
raviranjancom <erraviranjan030198@gmail.com>
2024-09-17 22:16:06 +0530

    Add 43-multiply-strings: Runtime Error - runtime NA - memory NA

---
+++ 43-multiply-strings/2024-09-17 22.16.06 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        long long a=0,b=0,ans=0;
+        a=stoi(num1);
+        b=stoi(num2);
+        ans=a*b;
+        string s=to_string(ans);
+        return s;
+    }
+};
