dd43d338f0e7cc1bf849e4f9ed08bbafd64a92ac
raviranjancom <erraviranjan030198@gmail.com>
2024-09-25 22:20:58 +0530

    Add 90-subsets-ii: Wrong Answer - runtime NA - memory NA

---
+++ 90-subsets-ii/2024-09-25 22.20.58 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,18 @@
+void subset2(vector<vector<int>> &ans,vector<int> &nums,vector<int> &v,int idx){
+    ans.push_back(v);
+    for(int i=idx;i<nums.size();i++){
+        if(i!=idx && nums[i]==nums[i-1]) continue;
+        v.push_back(nums[i]);
+        subset2(ans,nums,v,i+1);
+        v.pop_back();
+    }
+}
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> v;
+        subset2(ans,nums,v,0);
+        return ans;
+    }
+};
