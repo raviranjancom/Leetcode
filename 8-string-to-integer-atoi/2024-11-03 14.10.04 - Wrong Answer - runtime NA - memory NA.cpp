c2c7339c2a90f9cb9159a0a7031804f2c3885916
raviranjancom <erraviranjan030198@gmail.com>
2024-11-03 14:10:04 +0530

    Add 8-string-to-integer-atoi: Wrong Answer - runtime NA - memory NA

---
+++ 8-string-to-integer-atoi/2024-11-03 14.10.04 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int ans=0,sign=1,i=0;
+        bool flag=true,begin=false;
+        while(i<s.length() && s[i]==' ') {i++;}
+        while(i<s.length() && flag==true){
+            int n=(int)s[i]-48;
+            if(begin==false && s[i]=='-') sign=-1;
+            else if(-1 < n && n < 10){
+                begin=true;
+                if((INT_MAX-n)/10 < ans){
+                    if(sign==-1) return INT_MIN;
+                    else return INT_MAX;
+                }
+                ans*=10;
+                ans+=n;
+            }
+            else flag=false;
+            i++;
+        }
+        return sign*ans;
+    }
+};
