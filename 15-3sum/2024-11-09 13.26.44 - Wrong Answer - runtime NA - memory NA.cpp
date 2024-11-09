3b6c5c14dbc7bdf805e92743582110639d5d0171
raviranjancom <erraviranjan030198@gmail.com>
2024-11-09 13:26:44 +0530

    Add 15-3sum: Wrong Answer - runtime NA - memory NA

---
+++ 15-3sum/2024-11-09 13.26.44 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        set<vector<int>> st;
+        int n=nums.size();
+        map<int,int> mp;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                vector<int> ds;
+                int k=nums[i]+nums[j];
+                k*=-1;
+                if(mp.find(k)==mp.end()) continue;
+                else{
+                    if(mp[k]>1){
+                        ds.push_back(nums[i]);
+                        ds.push_back(nums[j]);
+                        ds.push_back(k);
+                        sort(ds.begin(),ds.end());
+                    }
+                    st.insert(ds);
+                }
+                
+            }
+        }
+        for(auto it:st){
+            if(it.size()==0) continue;
+            else ans.push_back(it);
+        }
+        return ans;
+    }
+};
