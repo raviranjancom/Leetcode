686fde680863e07510d07c48d557ba2435f51adc
raviranjancom <erraviranjan030198@gmail.com>
2025-01-03 21:33:54 +0530

    Add 142-linked-list-cycle-ii: Accepted - runtime 3ms - memory 11.4MB

---
+++ 142-linked-list-cycle-ii/2025-01-03 21.33.54 - Accepted - runtime 3ms - memory 11.4MB.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow=head, *fast=head;
+        if (!head || !head->next) return NULL;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(fast==slow){
+                slow=head;
+                while(slow!=fast){
+                    fast=fast->next;
+                    slow=slow->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+};
