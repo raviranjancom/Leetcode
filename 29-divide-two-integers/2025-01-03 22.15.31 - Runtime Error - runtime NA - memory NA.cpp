9e1138447fce204e91d2232fd9a955ebd58ac46e
raviranjancom <erraviranjan030198@gmail.com>
2025-01-03 22:15:31 +0530

    Add 29-divide-two-integers: Runtime Error - runtime NA - memory NA

---
+++ 29-divide-two-integers/2025-01-03 22.15.31 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        int sign;
+        if((dividend<0 &&divisor<0) || (dividend>0 &&divisor>0)) sign=1;
+        else sign=-1;
+        dividend=abs(dividend);
+        divisor=abs(divisor);
+        if(dividend==divisor) return sign*1;
+        int ans=0,x=divisor;;
+        while(dividend>divisor && ans<=INT_MAX && ans>=INT_MIN){
+            divisor+=x;
+            ans++;
+        }
+        return sign*ans;
+    }
+};
