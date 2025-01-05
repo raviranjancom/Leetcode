d579329b570cbfc612fdc58bbd224d466999097a
raviranjancom <erraviranjan030198@gmail.com>
2025-01-05 09:14:01 +0530

    Add 3634-find-mirror-score-of-a-string: Wrong Answer - runtime NA - memory NA

---
+++ 3634-find-mirror-score-of-a-string/2025-01-05 09.14.01 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long calculateScore(string s) {
+        long long ans=0,b=0;
+        int n=s.size();
+        set<int > st;
+        for(int i=1;i<n;i++){
+            int v=(int)s[i]-97;
+            for(int j=0;j<i;j++){
+                if(st.find(j)==st.end()){
+                    int u=(int)s[j]-97;
+                    if(u+v==25){
+                        ans+=(i-j);
+                        st.insert(j);
+                        st.insert(i);
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
