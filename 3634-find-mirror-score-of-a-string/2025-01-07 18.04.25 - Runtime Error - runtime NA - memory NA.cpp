493c9438c2b68d3ad320b14063dfb2a8303d1450
raviranjancom <erraviranjan030198@gmail.com>
2025-01-07 18:04:25 +0530

    Add 3634-find-mirror-score-of-a-string: Runtime Error - runtime NA - memory NA

---
+++ 3634-find-mirror-score-of-a-string/2025-01-07 18.04.25 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long calculateScore(string s) {
+        int n=s.size();
+        int score=0;
+        unordered_map<char, stack<int>> ds;
+        for(int i=0;i<n;i++){
+            char ch=s[i];
+            char x='z' - (ch - 'a');
+            if(ds[x].empty()){
+                ds[ch].push(i);
+            }
+            else{
+                score+=(i-ds[x].top());
+                ds[x].pop();
+            }
+        }
+        return score;
+    }
+};
