c3ff87b249e78fc340080b954aafd36d44c4f7b2
raviranjancom <erraviranjan030198@gmail.com>
2025-01-09 09:15:43 +0530

    Add 2292-counting-words-with-a-given-prefix: Accepted - runtime 0ms - memory 13MB

---
+++ 2292-counting-words-with-a-given-prefix/2025-01-09 09.15.43 - Accepted - runtime 0ms - memory 13MB.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int n=words.size(),ans=0;
+        for(int i=0;i<n;i++){
+            if(words[i].find(pref)==0) ans++;
+        }
+        return ans;
+    }
+};
