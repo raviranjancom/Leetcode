6f77a59677aa1244610695a5e749f7ff348d2b51
raviranjancom <erraviranjan030198@gmail.com>
2025-01-28 11:21:02 +0530

    Add 503-next-greater-element-ii: Accepted - runtime 0ms - memory 29MB

---
+++ 503-next-greater-element-ii/2025-01-28 11.21.02 - Accepted - runtime 0ms - memory 29MB.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        stack<int> st;
+        vector<int> ans;
+        for(int i=2*n-2;i>=0;i--){
+            while(!st.empty() && st.top() <= nums[i%n]){
+                st.pop();
+            }
+            if(i>=n){
+                st.push(nums[i%n]);
+            }
+            else{
+                if(st.empty()) ans.push_back(-1);
+                else{
+                    ans.push_back(st.top());
+                }
+                st.push(nums[i]);
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
