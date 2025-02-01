80493e7680401da576dcda5b4bcb5a57f2de248e
raviranjancom <erraviranjan030198@gmail.com>
2025-02-01 13:11:02 +0530

    Add 1146-greatest-common-divisor-of-strings: Wrong Answer - runtime NA - memory NA

---
+++ 1146-greatest-common-divisor-of-strings/2025-02-01 13.11.02 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        int n=str1.size();
+        int m=str2.size();
+        string ans;
+        if(str1[0]==str2[0]) ans+=str1[0];
+        else return ans;
+        int i=1;
+        while(i<n && i<m){
+            if(str1[i] == str2[i] && str1[i]!=ans[0]){
+                ans+=str1[i];
+                
+            }
+            else break;
+            
+            i++;
+        }
+        return ans;
+    }
+};
