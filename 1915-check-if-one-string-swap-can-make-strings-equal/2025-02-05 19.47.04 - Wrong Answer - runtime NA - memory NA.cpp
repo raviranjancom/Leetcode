78a8f4ea3241861a34b56fd5b49a28ff59ccaee0
raviranjancom <erraviranjan030198@gmail.com>
2025-02-05 19:47:04 +0530

    Add 1915-check-if-one-string-swap-can-make-strings-equal: Wrong Answer - runtime NA - memory NA

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/2025-02-05 19.47.04 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int n=s1.size();
+        int m=s2.size();
+        if(s1==s2) return true;
+        else if(n!=m) return false;
+        else{
+            int a=-1,b=-1;
+            int i=0;
+            while(i<n && a==-1){
+                if(s1[i]!=s2[i]){
+                    a=b;
+                    b=i;
+                }
+                i++;
+            }
+            if(a==-1 || b==-1) return false;
+            if(s1[a]==s2[b] && s1[b]==s2[a]) return true;
+        }
+        return false;
+    }
+};
