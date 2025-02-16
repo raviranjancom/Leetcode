7c367ee220d64a4ff3689f9f47e928f1b4ec6fda
raviranjancom <erraviranjan030198@gmail.com>
2025-02-17 01:08:48 +0530

    Add 402-remove-k-digits: Wrong Answer - runtime NA - memory NA

---
+++ 402-remove-k-digits/2025-02-17 01.08.48 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        stack<char> st;
+        int n=num.size(),i=0;
+        for(i=0;i<n;i++){
+            if(k<=0) break;
+            while(!st.empty() && st.top() > num[i] && k>0){
+                st.pop();
+                k--;
+            }
+            if(st.empty() && num[i]=='0'){
+                continue;
+            }
+            else{
+                st.push(num[i]);
+            }
+        }
+        while(i<n){
+            st.push(num[i]);
+            i++;
+        }
+        num="";
+        while(!st.empty()){
+            int x=st.top();
+            num+=x;
+            st.pop();
+        }
+        reverse(num.begin(),num.end());
+        if(num=="") num="0";
+        return num;
+    }
+};
