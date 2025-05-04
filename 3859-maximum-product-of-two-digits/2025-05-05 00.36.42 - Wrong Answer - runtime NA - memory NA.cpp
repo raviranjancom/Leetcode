0d9dc5b1b175b9f6bc06f49f50081f9d0946a567
raviranjancom <erraviranjan030198@gmail.com>
2025-05-05 00:36:42 +0530

    Add 3859-maximum-product-of-two-digits: Wrong Answer - runtime NA - memory NA

---
+++ 3859-maximum-product-of-two-digits/2025-05-05 00.36.42 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        vector<int> v;
+        int num=n;
+        while(num>0){
+            int last=num%10;
+            if(last!=0) v.push_back(last);
+            num=num/10;
+        }
+        sort(v.begin(),v.end());
+        reverse(v.begin(),v.end());
+        return v.size()>1 ? v[0]*v[1] : v[0];
+    }
+};
