cc154dca17b07012f723a2144e2ca8df36676465
raviranjancom <erraviranjan030198@gmail.com>
2025-06-08 20:34:00 +0530

    Add 424-longest-repeating-character-replacement: Time Limit Exceeded - runtime NA - memory NA

---
+++ 424-longest-repeating-character-replacement/2025-06-08 20.34.00 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n=s.length();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int hash[26]={0};
+            int fre=0;
+            for(int j=i;j<n;j++){
+                hash[s[j] - 'A']++;
+                fre=max(fre,hash[s[j] - 'A']);
+                int x=j-i+1 - fre;
+                if(x<=k){
+                    ans=max(ans,j-i+1);
+                }
+                else break;
+            }
+        }
+        return ans;
+    }
+};
