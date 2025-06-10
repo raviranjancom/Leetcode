07a3592d1fc08a486b35334d846718bc234f95e4
raviranjancom <erraviranjan030198@gmail.com>
2025-06-10 19:27:44 +0530

    Add 56-merge-intervals: Wrong Answer - runtime NA - memory NA

---
+++ 56-merge-intervals/2025-06-10 19.27.44 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        vector<vector<int>> ans;
+        int i=1;
+        ans.push_back(intervals[0]);
+        while(i<n){
+            if(ans.back()[1] >= intervals[i][0]){
+                vector<int> x=ans.back();
+                ans.pop_back();
+                ans.push_back({x[0], intervals[i][1]});
+            }
+            else{
+                ans.push_back(intervals[i]);
+            }
+            i++;
+        }
+        return ans;
+    }
+};
