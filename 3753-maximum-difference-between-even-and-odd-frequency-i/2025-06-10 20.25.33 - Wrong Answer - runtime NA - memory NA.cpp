3c37a56ce7b81651ba3b3b216d5b76a115a4023f
raviranjancom <erraviranjan030198@gmail.com>
2025-06-10 20:25:33 +0530

    Add 3753-maximum-difference-between-even-and-odd-frequency-i: Wrong Answer - runtime NA - memory NA

---
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/2025-06-10 20.25.33 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxDifference(string s) {
+        int n=s.length();
+        int min_odd=INT_MAX,min_even=INT_MAX;
+        int max_odd=INT_MIN, max_even=INT_MIN;
+        map<char,int> hash;
+        for(int i=0;i<n;i++){
+            hash[s[i]]++;
+        }
+        for(auto i : hash){
+            if(i.second%2==1){
+                min_odd=min(min_odd, i.second);
+                max_odd=max(max_odd, i.second);
+            }
+            else{
+                min_even=min(min_even, i.second);
+                max_even=max(max_even, i.second);
+            }
+        }
+        int x=abs(min_odd - max_even);
+        int y=abs(max_odd - min_even);
+        return (x>y)?(min_odd - max_even):(max_odd - min_even);
+    }
+};
