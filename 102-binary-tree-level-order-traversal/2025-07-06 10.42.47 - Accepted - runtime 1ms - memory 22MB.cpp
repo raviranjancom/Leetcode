a63edfe8e476fa65e801e6cacafd0260e36bfb11
raviranjancom <erraviranjan030198@gmail.com>
2025-07-06 10:42:47 +0530

    Add 102-binary-tree-level-order-traversal: Accepted - runtime 1ms - memory 22MB

---
+++ 102-binary-tree-level-order-traversal/2025-07-06 10.42.47 - Accepted - runtime 1ms - memory 22MB.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void levelorder(TreeNode* r, vector<vector<int>> &ans){
+        queue<TreeNode *> q;
+        if(r){
+            q.push({r});
+        }
+        while(!q.empty()){
+            queue<TreeNode *> t=q;
+            vector<int> ds;
+            while(!t.empty()){
+                TreeNode* p=t.front();
+                t.pop();
+                q.pop();
+                ds.push_back(p->val);
+                if(p->left) q.push(p->left);
+                if(p->right) q.push(p->right);
+            }
+            ans.push_back(ds);
+        }
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        levelorder(root, ans);
+        return ans;
+    }
+};
