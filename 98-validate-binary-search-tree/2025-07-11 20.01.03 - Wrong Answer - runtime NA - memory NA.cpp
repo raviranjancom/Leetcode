7da182c9db80378648e5ce20049f5cf2ae5c7b20
raviranjancom <erraviranjan030198@gmail.com>
2025-07-11 20:01:03 +0530

    Add 98-validate-binary-search-tree: Wrong Answer - runtime NA - memory NA

---
+++ 98-validate-binary-search-tree/2025-07-11 20.01.03 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool fun(TreeNode* root, int low, int high){
+        if(!root){
+            return true;
+        }
+        else if(root->val <= low || root->val >= high){
+            return false;
+        }
+        return fun(root->left,low,root->val) && fun(root->right,root->val,high);
+    }
+    bool isValidBST(TreeNode* root) {
+        return fun(root,INT_MIN,INT_MAX);
+    }
+};
