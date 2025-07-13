64696ec081b354fee7d683d302677e79e27ebe20
raviranjancom <erraviranjan030198@gmail.com>
2025-07-13 08:11:15 +0530

    Add 3931-process-string-with-special-operations-i: Accepted - runtime 0ms - memory 39.8MB

---
+++ 3931-process-string-with-special-operations-i/2025-07-13 08.11.15 - Accepted - runtime 0ms - memory 39.8MB.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string processStr(string s) {
+        int n=s.length();
+        string ans;
+        for(int i=0;i<n;i++){
+            if(s[i]>=97 && s[i]<=122){
+                ans.push_back(s[i]);
+            }
+            else if(s[i]=='*'){
+                if(ans.length()>0){
+                    ans.pop_back();
+                }
+                else{
+                    continue;
+                }
+            }
+            else if(s[i]=='#'){
+                ans=ans+ans;
+            }
+            else if(s[i]=='%'){
+                reverse(ans.begin(),ans.end());
+            }
+        }
+        return ans;
+    }
+};
