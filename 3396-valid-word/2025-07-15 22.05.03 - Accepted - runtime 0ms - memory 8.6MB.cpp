9f21f8e3732ed19655100d1002fa06495ba83391
raviranjancom <erraviranjan030198@gmail.com>
2025-07-15 22:05:03 +0530

    Add 3396-valid-word: Accepted - runtime 0ms - memory 8.6MB

---
+++ 3396-valid-word/2025-07-15 22.05.03 - Accepted - runtime 0ms - memory 8.6MB.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool check(char ch, string list){
+        for(char it:list){
+            if(ch==it || ch=='\0') return true;
+        }
+        return false;
+    }
+    
+    bool isValid(string word) {
+        string vlist="aeiouAEIOU";
+        string clist="bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+        
+        int n=word.length();
+        if(n<3) return false;
+        bool vowel=false,consonent=false,digits=false,letters=false;
+        for(int i=0;i<n;i++){
+            if((word[i]>=65 && word[i]<=90) || (word[i]>=97 && word[i]<=122)){
+                letters=true;
+            }
+            if(check(word[i],vlist)){
+                vowel=true;
+            }
+            if(check(word[i],clist)){
+                consonent=true;
+            }
+            if(word[i]>=48 && word[i]<=57){
+                digits=true;
+            }
+            if((word[i]>=65 && word[i]<=90) || (word[i]>=97 && word[i]<=122 || (word[i]>=48 && word[i]<=57))){
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+        return vowel*consonent*(digits||letters);
+    }
+};
