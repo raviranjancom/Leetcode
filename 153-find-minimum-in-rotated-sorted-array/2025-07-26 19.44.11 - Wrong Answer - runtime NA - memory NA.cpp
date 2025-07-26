b0f4d45579893e7fb0dd161168f6db1a6c170457
raviranjancom <erraviranjan030198@gmail.com>
2025-07-26 19:44:11 +0530

    Add 153-find-minimum-in-rotated-sorted-array: Wrong Answer - runtime NA - memory NA

---
+++ 153-find-minimum-in-rotated-sorted-array/2025-07-26 19.44.11 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n=nums.size();
+        int l=0,r=n-1;
+        while(l<r){
+            int m=(l+r)/2;
+            if(nums[m]<nums[m+1]){
+                r=m;
+            }else{
+                l=m+1;
+            }
+        }
+        return nums[l];
+    }
+};
