2755b5a11e733410fbdd416cc2bc8ee68c8ef695
raviranjancom <erraviranjan030198@gmail.com>
2025-08-04 00:10:03 +0530

    Add 2229-maximum-fruits-harvested-after-at-most-k-steps: Wrong Answer - runtime NA - memory NA

---
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2025-08-04 00.10.03 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int n=fruits.size();
+        vector<int> position(n),prefixsum(n);
+        for(int i=0;i<n;i++){
+            position[i]=fruits[i][0];
+            prefixsum[i]=(i>0)?prefixsum[i-1]+fruits[i][1]:0;
+        }
+        int ans=0;
+        for(int d=0;d<=k/2;d++){
+            int remain=k-2*d;
+            int i=startPos-d;
+            int j=startPos+remain;
+            int left=lower_bound(position.begin(),position.end(),i)-position.begin();
+            int right=upper_bound(position.begin(), position.end(),j)-position.begin()-1;
+
+            if(left<=right){
+                int sum=prefixsum[right]-(left>0?prefixsum[left-1]:0);
+                ans=max(ans,sum);
+            }
+
+            i=startPos-remain;
+            j=startPos+d;
+            left=lower_bound(position.begin(),position.end(),i)-position.begin();
+            right=upper_bound(position.begin(), position.end(),j)-position.begin()-1;
+
+            if(left<=right){
+                int sum=prefixsum[right]-(left>0?prefixsum[left-1]:0);
+                ans=max(ans,sum);
+            }
+            
+        }
+        return ans;
+    }
+};
