d770cacbcea45a8ed44dfa2421d6963ff440546d
raviranjancom <erraviranjan030198@gmail.com>
2025-08-03 20:25:29 +0530

    Add 2229-maximum-fruits-harvested-after-at-most-k-steps: Runtime Error - runtime NA - memory NA

---
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2025-08-03 20.25.29 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int n=fruits.size();
+        int startindex=0;
+        while(startindex<n && fruits[startindex][0] <= startPos){
+            startindex++;
+        }
+        if(startPos==fruits[startindex-1][0]){
+            startindex--;
+        }
+        else{
+            fruits.insert(fruits.begin()+startindex,{startPos,0});
+        }
+        fruits.push_back({n,0});
+        n++;
+        int left=startindex,right=startindex;
+        int sum=fruits[startindex][1],ans=0;
+        while(left>0 && fruits[startindex][0]-fruits[left-1][0]<=k){
+            left--;
+            sum+=fruits[left][1];
+            ans=max(ans,sum);
+        }
+        while(right<n-1){
+            cout<<" "<<sum<<" "<<ans<<" "<<fruits[left][0]<<" "<<fruits[right][0]<<endl;
+            
+            if(startPos-fruits[left][0] + fruits[right+1][0]-fruits[left][0]<=k){
+                right++;
+                sum+=fruits[right][1];
+                if(startPos-fruits[right+1][0]>k) break;
+            }
+            else{
+                sum-=fruits[left][1];
+                if(left<startindex) left++;
+                else{
+                    break;
+                }
+            }
+            ans=max(ans,sum);
+        }
+        return ans;
+    }
+};
