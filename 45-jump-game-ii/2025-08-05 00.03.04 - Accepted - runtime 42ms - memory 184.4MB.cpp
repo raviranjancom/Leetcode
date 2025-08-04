aa87776b10167ce66d20cda192ff29eadcc45509
raviranjancom <erraviranjan030198@gmail.com>
2025-08-05 00:03:04 +0530

    Add 45-jump-game-ii: Accepted - runtime 42ms - memory 184.4MB

---
+++ 45-jump-game-ii/2025-08-05 00.03.04 - Accepted - runtime 42ms - memory 184.4MB.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxf(vector<int> arr, int l, int r){
+        int m=l;
+        for(int i=l;i<=r;i++){
+            if(arr[m]<arr[i]) m=i;
+        }
+        return m;
+    }
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return 0;
+        vector<int> steps(n);
+        for(int i=0;i<n;i++){
+            steps[i]=i+nums[i];
+        }
+        // calculate
+        int ans=0;
+        int i=0;
+        while(i<n){
+            int range=steps[i];
+            if(range>=n-1) return ++ans;
+            int nextindex=maxf(steps,i+1,range);
+            i=nextindex;
+            ans++;
+        }
+        return ans;
+    }
+};
