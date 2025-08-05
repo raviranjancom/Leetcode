5f41f802279d60d73831d0ebb7ae702f19bf56a0
raviranjancom <erraviranjan030198@gmail.com>
2025-08-05 16:25:17 +0530

    Add 3790-fruits-into-baskets-ii: Runtime Error - runtime NA - memory NA

---
+++ 3790-fruits-into-baskets-ii/2025-08-05 16.25.17 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool check(vector<int> &q, int target){
+        for(int i=0;i<q.size();i++){
+            if(target>=q[i]){
+                q.erase(q.begin() + i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n=fruits.size();
+        vector<int> q;
+        int i=0,j=0;
+        while(j<n){
+            int x=check(q,baskets[j]);
+            if(x){
+                j++;
+                if(i>=n) break;
+            }
+            else if(fruits[i]<=baskets[j]){
+                i++;
+                j++;
+            }
+            else{
+                q.push_back(fruits[i]);
+                i++;
+            }
+        }
+        for(auto it:q){
+            cout<<it<<" ";
+        }
+        return q.size();
+    }
+};
