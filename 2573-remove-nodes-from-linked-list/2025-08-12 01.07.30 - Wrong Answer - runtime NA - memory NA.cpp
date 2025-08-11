4efffac2962bfe722ff067a54a8b8082974938c0
raviranjancom <erraviranjan030198@gmail.com>
2025-08-12 01:07:30 +0530

    Add 2573-remove-nodes-from-linked-list: Wrong Answer - runtime NA - memory NA

---
+++ 2573-remove-nodes-from-linked-list/2025-08-12 01.07.30 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        stack<ListNode *> st;
+        ListNode *r=head;
+        while(r){
+            st.push(r);
+            r=r->next;
+        }
+        int m=0;
+        r=st.top();
+        st.pop();
+        m=max(m,r->val);
+        while(!st.empty()){
+            ListNode *x=st.top();
+            st.pop();
+            if(x->val>=m){
+                x->next=r;
+                r=x;
+            }
+        }
+        return r;
+    }
+};
