088926a4ded54913ba1330b49bd5f180e8441053
raviranjancom <erraviranjan030198@gmail.com>
2025-08-12 18:56:30 +0530

    Add 2882-ways-to-express-an-integer-as-sum-of-powers: Wrong Answer - runtime NA - memory NA

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2025-08-12 18.56.30 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int fun(int num,int x,int sum,vector<vector<int>> &dp){
+        if(sum==0){
+            return 1;
+        }
+        else if(sum<0){
+            return 0;
+        }
+        else if(pow(num,x)>sum){
+            return 0;
+        }
+        else if(dp[num][sum]!=-1){
+            return dp[num][sum];
+        }
+        int p=pow(num,x);
+        sum-=p;
+        int a=fun(num+1,x,sum,dp);
+        sum+=p;
+        int b=fun(num+1,x,sum,dp);
+        return dp[num][sum]=a+b;
+    }
+    int numberOfWays(int n, int x) {
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return fun(1,x,n,dp);
+    }
+};
