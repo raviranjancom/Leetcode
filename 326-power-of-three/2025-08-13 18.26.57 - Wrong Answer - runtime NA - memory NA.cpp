0f9b78997f0e743c6665ccad2d9cc905a3155082
raviranjancom <erraviranjan030198@gmail.com>
2025-08-13 18:26:57 +0530

    Add 326-power-of-three: Wrong Answer - runtime NA - memory NA

---
+++ 326-power-of-three/2025-08-13 18.26.57 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<1) return false;
+        int num=1;
+        for(int i=2;i<21;i++){
+            if(abs(n)==num) return true;
+            if(num>abs(n)) return false;
+            num*=3;
+        }
+        return false;
+    }
+};
