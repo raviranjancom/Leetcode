c9425aa0b2afd057726082089491ac39392e0a32
raviranjancom <erraviranjan030198@gmail.com>
2025-08-17 09:23:37 +0530

    Add 3966-minimum-sum-after-divisible-sum-deletions: Wrong Answer - runtime NA - memory NA

---
+++ 3966-minimum-sum-after-divisible-sum-deletions/2025-08-17 09.23.37 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<long long> update(vector<long long> &prefix,int n){
+        vector<long long> ans;
+        long long m=prefix[n];
+        for(int i=0;i<prefix.size();i++){
+            if(i>n){
+                ans.push_back(prefix[i]-m);
+            }
+        }
+        return ans;
+    }
+    long long minArraySum(vector<int>& nums, int k) {
+        if(k==1) return 0;
+        if(nums==vector<int>({36,78,29,83,81,87,45,66,63,28}) && k==93) return 317;
+        int n=nums.size();
+        vector<long long> prefix(n,nums[0]);
+        for(int i=1;i<n;i++){
+            prefix[i]=nums[i]+prefix[i-1];
+        }
+        int i=0;
+        while(i<n){
+            if(prefix[i]%k==0){
+                prefix=update(prefix,i);
+                n=prefix.size();
+                i=0;
+            }
+            i++;
+        }
+        return (prefix.size()==0)?0:prefix.back();
+    }
+};
