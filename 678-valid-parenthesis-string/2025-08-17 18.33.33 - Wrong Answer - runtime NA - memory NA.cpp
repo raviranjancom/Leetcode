c195c35c9c5a8781ed887310f843650b26862c12
raviranjancom <erraviranjan030198@gmail.com>
2025-08-17 18:33:33 +0530

    Add 678-valid-parenthesis-string: Wrong Answer - runtime NA - memory NA

---
+++ 678-valid-parenthesis-string/2025-08-17 18.33.33 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n=s.length();
+        stack<char> st;
+        for(int i=0;i<n;i++){
+            if(s[i]==')'){
+                stack<char> temp_stack;
+                while(!st.empty() && st.top()!='('){
+                    temp_stack.push(st.top());
+                    st.pop();
+                }
+                if(!st.empty()){
+                    st.pop();
+                }
+                else{
+                    if(!temp_stack.empty()){
+                        temp_stack.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                while(!temp_stack.empty()){
+                    st.push(temp_stack.top());
+                    temp_stack.pop();
+                }
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        return true;
+    }
+};
