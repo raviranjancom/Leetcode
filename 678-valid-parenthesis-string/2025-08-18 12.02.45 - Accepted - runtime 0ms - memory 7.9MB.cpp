4f96d2002135dd5dfba7f77e5c25c566a6a2ad8e
raviranjancom <erraviranjan030198@gmail.com>
2025-08-18 12:02:45 +0530

    Add 678-valid-parenthesis-string: Accepted - runtime 0ms - memory 7.9MB

---
+++ 678-valid-parenthesis-string/2025-08-18 12.02.45 - Accepted - runtime 0ms - memory 7.9MB.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        // greedly maintain thr range between the values [-1 ,0 ,1]
+        int n=s.length();
+        int min=0,max=0;
+        for(int i=0;i<n;i++){
+            if(max<0) return false;
+            if(s[i]=='('){
+                min++;
+                max++;
+            }
+            else if(s[i]=='*'){
+                max++;
+                if(min>0) min--;
+            }
+            else{
+                if(min>0) min--;
+                max--;
+            }
+        }
+        if(min==0 && max>=0) return true;
+        return false;
+    }
+};
