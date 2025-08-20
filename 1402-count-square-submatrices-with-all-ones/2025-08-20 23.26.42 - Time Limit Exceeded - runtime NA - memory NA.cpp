2f63b2263453d033146e6b0318ed10713ab65a8e
raviranjancom <erraviranjan030198@gmail.com>
2025-08-20 23:26:42 +0530

    Add 1402-count-square-submatrices-with-all-ones: Time Limit Exceeded - runtime NA - memory NA

---
+++ 1402-count-square-submatrices-with-all-ones/2025-08-20 23.26.42 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int min(int a,int b){
+        return(a<b)?a:b;
+    }
+    int fun(vector<vector<int>>& matrix,int n,int m,int i,int j){
+        if(i>=n || j>=m || matrix[i][j]==0){
+            return 0;
+        }
+        int right=1+fun(matrix,n,m,i,j+1);
+        int down=1+fun(matrix,n,m,i+1,j);
+        int diagonal=1+fun(matrix,n,m,i+1,j+1);
+        return min(right,min(down,diagonal));
+    }
+    int countSquares(vector<vector<int>>& matrix) {
+        int n=matrix.size(),m=matrix[0].size(),ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                ans+=fun(matrix,n,m,i,j);
+            }
+        }
+        return ans;
+    }
+};
