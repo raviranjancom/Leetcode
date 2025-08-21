9298d5cbf06bebd41c718bc80be3cb6d595a7ea8
raviranjancom <erraviranjan030198@gmail.com>
2025-08-21 23:56:24 +0530

    Add 135-candy: Wrong Answer - runtime NA - memory NA

---
+++ 135-candy/2025-08-21 23.56.24 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int solve(vector<int>& ratings){
+        int n=ratings.size();
+        int i=1,ans=1,peek=1,down=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                ans+=1;
+                i++;
+                peek=1;
+            }
+            while(i<n && ratings[i]>ratings[i-1]){
+                peek+=1;
+                ans+=peek;
+                i++;
+            }
+            while(i<n && ratings[i]<ratings[i-1]){
+                ans+=down;
+                down++;
+                i++;
+            }
+            if(peek<down){
+                ans+=down-peek;
+                peek=1;
+                down=1;
+            }
+        }
+        return ans;
+    }
+    int candy(vector<int>& ratings) {
+        return solve(ratings);
+    }
+};
