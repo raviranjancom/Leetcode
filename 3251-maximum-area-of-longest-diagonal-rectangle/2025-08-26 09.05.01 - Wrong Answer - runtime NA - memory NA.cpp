69f5b1a12d65d68d4011eedfc0fffba410272cbc
raviranjancom <erraviranjan030198@gmail.com>
2025-08-26 09:05:01 +0530

    Add 3251-maximum-area-of-longest-diagonal-rectangle: Wrong Answer - runtime NA - memory NA

---
+++ 3251-maximum-area-of-longest-diagonal-rectangle/2025-08-26 09.05.01 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double diagonal_length(vector<int> &ds){
+        return pow((ds[0]*ds[0] + ds[1]*ds[1]), 0.5);
+    }
+    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
+        int n=dimensions.size();
+        double max_len=0.0;
+        int area=0;
+        for(int i=0;i<n;i++){
+            double x=diagonal_length(dimensions[i]);
+            if(max_len<=x){
+                area=max(area,dimensions[i][0] * dimensions[i][1]);
+                max_len=x;
+            }
+        }
+        return area;
+    }
+};
