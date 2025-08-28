066bcc7a5303aa3ea0142cf2683101f5dda65a12
raviranjancom <erraviranjan030198@gmail.com>
2025-08-28 22:11:56 +0530

    Add 498-diagonal-traverse: Wrong Answer - runtime NA - memory NA

---
+++ 498-diagonal-traverse/2025-08-28 22.11.56 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int n=mat.size(),m=mat[0].size();
+        vector<int> ans;
+        // by cols
+        for(int col=0;col<m;col++){
+            int i=0,j=col;
+            while(j>=0 && i<=col && i<n){
+                ans.push_back(mat[i][j]);
+                i++;
+                j--;
+            }
+            if(col%2==0){
+                reverse(ans.end()-col-1,ans.end());
+            }
+        }
+
+        // by cols
+        for(int row=1;row<n;row++){
+            int i=row,j=m-1;
+            while(i<n && j>=0 && i>=0){
+                ans.push_back(mat[i][j]);
+                i++;
+                j--;
+            }
+            if(row%2==0)reverse(ans.end()-(n-row),ans.end());
+        }
+        return ans;
+    }
+};
