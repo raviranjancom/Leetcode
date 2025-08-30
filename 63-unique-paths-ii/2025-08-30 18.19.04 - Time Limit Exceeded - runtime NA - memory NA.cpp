d921e268ef71d8eb7fd99a2f0dad908be02a9c83
raviranjancom <erraviranjan030198@gmail.com>
2025-08-30 18:19:04 +0530

    Add 63-unique-paths-ii: Time Limit Exceeded - runtime NA - memory NA

---
+++ 63-unique-paths-ii/2025-08-30 18.19.04 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int solve(vector<vector<int>>& obstacleGrid,int cr,int cc,int n,int m){
+        if(cr>=n || cc>=m || obstacleGrid[cr][cc]==1){
+            return 0;
+        }
+        else if(cr==n-1 && cc==m-1){
+            return 1;
+        }
+        
+        return solve(obstacleGrid,cr+1,cc,n,m)+solve(obstacleGrid,cr,cc+1,n,m);
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n=obstacleGrid.size(),m=obstacleGrid[0].size();
+        return solve(obstacleGrid,0,0,n,m);
+    }
+};
