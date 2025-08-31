4a80bb607a02b990a2895aa5fd8cdb0427bcd808
raviranjancom <erraviranjan030198@gmail.com>
2025-08-31 23:49:35 +0530

    Add 224-basic-calculator: Accepted - runtime 0ms - memory 10.4MB

---
+++ 224-basic-calculator/2025-08-31 23.49.35 - Accepted - runtime 0ms - memory 10.4MB.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int n=s.length();
+        stack<int> st;
+        int ans=0;
+        int number=0;
+        int sign=1;
+        for(int i=0;i<n;i++){
+            if(isdigit(s[i])){
+                number=number*10 + (s[i]-'0');
+            }
+            else if(s[i]=='+'){
+                ans+=(number*sign);
+                number=0;
+                sign=1;
+            }
+            else if(s[i]=='-'){
+                ans+=(number*sign);
+                number=0;
+                sign=-1;
+            }
+            else if(s[i]=='('){
+                st.push(ans);
+                st.push(sign);
+                ans=0;
+                number=0;
+                sign=1;
+            }
+            else if(s[i]==')'){
+                ans+=(number*sign);
+                number=0;
+                sign=0;
+
+                int stack_sign=st.top(); st.pop();
+                int last_ans=st.top(); st.pop();
+
+                ans*=stack_sign;
+                ans+=last_ans;
+            }
+        }
+        ans+=(sign*number);
+        return ans;
+    }
+};
