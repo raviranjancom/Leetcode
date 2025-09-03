cff9256d25a28cf11701f4c33059b4ff9c062d8c
raviranjancom <erraviranjan030198@gmail.com>
2025-09-03 19:14:32 +0530

    Add 134-gas-station: Accepted - runtime 0ms - memory 112.4MB

---
+++ 134-gas-station/2025-09-03 19.14.32 - Accepted - runtime 0ms - memory 112.4MB.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n=gas.size(),sum=0;
+        int current_sum=0,previous_sum=0,index=0;
+        for(int i=0;i<n;i++){
+            // total sum
+            sum+=gas[i]-cost[i];
+
+            current_sum=previous_sum+(gas[i]-cost[i]);
+            previous_sum=current_sum;
+            
+            if(current_sum < 0){
+                current_sum=0;
+                previous_sum=0;
+                index=i+1;
+            }
+            
+        }
+        return (sum>=0)?index:-1;
+    }
+};
+
+
+// 3 1 1
+// 1 2 2
+// 
