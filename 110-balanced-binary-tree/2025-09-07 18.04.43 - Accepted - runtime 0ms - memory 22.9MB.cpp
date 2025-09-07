d4c78f682fad65920b5a838f8a83425645be7324
raviranjancom <erraviranjan030198@gmail.com>
2025-09-07 18:04:43 +0530

    Add 110-balanced-binary-tree: Accepted - runtime 0ms - memory 22.9MB

---
+++ 110-balanced-binary-tree/2025-09-07 18.04.43 - Accepted - runtime 0ms - memory 22.9MB.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode *r){
+        if(r==nullptr){
+            return 0;
+        }
+        return 1+max(height(r->left),height(r->right));
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root){
+            if(abs(height(root->left) - height(root->right))>1) return false;
+            bool x=isBalanced(root->left);
+            bool y=isBalanced(root->right);
+            return x & y;
+        }
+        return true;
+    }
+};
