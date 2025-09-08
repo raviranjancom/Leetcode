1deb6c8fad9e25fb2d801a14a3f695fe8f6a5c76
raviranjancom <erraviranjan030198@gmail.com>
2025-09-08 17:24:15 +0530

    Add 124-binary-tree-maximum-path-sum: Wrong Answer - runtime NA - memory NA

---
+++ 124-binary-tree-maximum-path-sum/2025-09-08 17.24.15 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum(TreeNode *r){
+        if(!r) return 0;
+        if(r->left==nullptr && r->right==nullptr){
+            return r->val;
+        }
+        int left=sum(r->left);
+        int right=sum(r->right);
+        return max(r->val+left+right,max(left,right));
+    }
+
+    int maxPathSum(TreeNode* root){
+        return sum(root);
+    }
+};
