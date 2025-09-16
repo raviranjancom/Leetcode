9456cffc1209f2fc75084bc1243c98d6a88fab1f
raviranjancom <erraviranjan030198@gmail.com>
2025-09-16 13:38:14 +0530

    Add 2307-replace-non-coprime-numbers-in-array: Wrong Answer - runtime NA - memory NA

---
+++ 2307-replace-non-coprime-numbers-in-array/2025-09-16 13.38.14 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool GCD(int a,int b){
+        int n=min(a,b);
+        for(int i=2;i<=n;i++){
+            if(a%i==0 && b%i==0) return true;
+        }
+        return false;
+    }
+    int LCM(int a, int b){
+        long long m=max(a,b);
+        long long n=1LL*a*b;
+        for(long long i=m;i<=n;i++){
+            if(i%a==0 && i%b==0){
+                return i;
+            }
+        }
+        return n;
+    }
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int t=ans.back();
+            if(GCD(t,nums[i])){
+                ans.pop_back();
+                int x=LCM(t,nums[i]);
+                ans.push_back(x);
+            }
+            else{
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
