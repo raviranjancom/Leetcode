ff2e9145559a1a798521fbb61fe5ecde6efc1e48
raviranjancom <erraviranjan030198@gmail.com>
2025-09-16 14:06:02 +0530

    Add 2307-replace-non-coprime-numbers-in-array: Accepted - runtime 9ms - memory 125.1MB

---
+++ 2307-replace-non-coprime-numbers-in-array/2025-09-16 14.06.02 - Accepted - runtime 9ms - memory 125.1MB.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    // bool GCD(int a, int b) {
+    //     while (b != 0) {
+    //         int temp = a % b;
+    //         a = b;
+    //         b = temp;
+    //     }
+    //     return a>1;
+    // }
+
+    // int LCM(int a, int b){
+    //     long long m=max(a,b);
+    //     long long n=1LL*a*b;
+    //     for(long long i=m;i<=n;i++){
+    //         if(i%a==0 && i%b==0){
+    //             return i;
+    //         }
+    //     }
+    //     return n;
+    // }
+
+    bool GCD(int a, int b) {  // true if not coprime
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a > 1;
+    }
+
+    int gcdVal(int a, int b) {  // numeric gcd for LCM
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+    long long LCM(long long a, long long b) {
+        return (a / gcdVal(a, b)) * b;
+    }
+    
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            while(ans.size()>0 && GCD(ans.back(),nums[i])){
+                nums[i]=LCM(ans.back(),nums[i]);
+                ans.pop_back();
+            }
+            ans.push_back(nums[i]);
+        }
+        return ans;
+    }
+};
