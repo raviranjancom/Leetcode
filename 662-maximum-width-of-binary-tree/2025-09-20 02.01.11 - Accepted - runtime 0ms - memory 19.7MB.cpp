a7c70ad32682f43e69bfa1f347822172b6b94f2b
raviranjancom <erraviranjan030198@gmail.com>
2025-09-20 02:01:11 +0530

    Add 662-maximum-width-of-binary-tree: Accepted - runtime 0ms - memory 19.7MB

---
+++ 662-maximum-width-of-binary-tree/2025-09-20 02.01.11 - Accepted - runtime 0ms - memory 19.7MB.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if(!root) return 0;
+        queue<pair<TreeNode *,int>> q;
+        q.push({root, 0});
+        int ans=0;
+        while(!q.empty()){
+            int n=q.size();
+            int min=q.front().second;
+            int start,last,curr=0;
+            for(int i=0;i<n;i++){
+                auto x=q.front(); q.pop();
+                TreeNode *node=x.first;
+                long long idx=1LL*x.second - 1LL*min;
+
+                if(node->left){
+                    q.push({node->left, 2*idx+1});
+                }
+                if(node->right){
+                    q.push({node->right, 2*idx+2});
+                }
+                if(i==0) start=idx;
+                if(i==n-1) last=idx;
+            }
+            ans=max(ans, last-start+1);
+        }
+
+        return ans;
+    }
+};
