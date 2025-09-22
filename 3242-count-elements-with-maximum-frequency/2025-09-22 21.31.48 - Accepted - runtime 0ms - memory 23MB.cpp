1615bd67ae91cdac801b80faba237c57d1fbbfcf
raviranjancom <erraviranjan030198@gmail.com>
2025-09-22 21:31:48 +0530

    Add 3242-count-elements-with-maximum-frequency: Accepted - runtime 0ms - memory 23MB

---
+++ 3242-count-elements-with-maximum-frequency/2025-09-22 21.31.48 - Accepted - runtime 0ms - memory 23MB.cpp
@@ -0,0 +1,16 @@
+#include<cstdint>
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        int8_t hash[101]={0},m=-1,ans=0;
+        for(int8_t i=0;i<nums.size();i++){
+            hash[nums[i]]++;
+            m=max(m,hash[nums[i]]);
+        }
+
+        for(int8_t i=0;i<=100;i++){
+            if(hash[i]==m) ans+=hash[i];
+        }
+        return ans;
+    }
+};
