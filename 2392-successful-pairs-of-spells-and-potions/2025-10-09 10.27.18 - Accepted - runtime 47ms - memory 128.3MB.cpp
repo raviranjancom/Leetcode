1bf09434e4b1e639f5728cae41a1122cef0d0fdf
raviranjancom <erraviranjan030198@gmail.com>
2025-10-09 10:27:18 +0530

    Add 2392-successful-pairs-of-spells-and-potions: Accepted - runtime 47ms - memory 128.3MB

---
+++ 2392-successful-pairs-of-spells-and-potions/2025-10-09 10.27.18 - Accepted - runtime 47ms - memory 128.3MB.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n=spells.size(),m=potions.size();
+        sort(potions.begin(),potions.end());
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            int l=0,r=m-1;
+            while(l<r){
+                int mid=(l+r)/2;
+                if(1LL*potions[mid]*spells[i]>=success){
+                    r=mid;
+                }
+                else{
+                    l=mid+1;
+                }
+            }
+            if(1LL*potions[r]*spells[i]>=success){
+                ans.push_back(m-r);
+            }
+            else{
+                ans.push_back(0);
+            }
+        }
+        return ans;
+    }
+};
