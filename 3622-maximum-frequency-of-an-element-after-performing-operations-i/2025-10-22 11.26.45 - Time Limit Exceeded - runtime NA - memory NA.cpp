ee38487b587e8843a8564b48743721ac66890446
raviranjancom <erraviranjan030198@gmail.com>
2025-10-22 11:26:45 +0530

    Add 3622-maximum-frequency-of-an-element-after-performing-operations-i: Time Limit Exceeded - runtime NA - memory NA

---
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/2025-10-22 11.26.45 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int lower_bound(vector<int>& nums,int l,int r, int t){
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]<t){
+                l=mid+1;
+            }
+            else{
+                r=mid;
+            }
+        }
+        return l;
+    }
+    int upper_bound(vector<int>& nums,int l,int r, int t){
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]<=t){
+                l=mid+1;
+            }
+            else{
+                r=mid;
+            }
+        }
+        return l;
+    }
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        int n=nums.size(),ans=1;
+        sort(nums.begin(),nums.end());
+        unordered_map<int,int> mp;
+        for(auto it:nums){
+            mp[it]++;
+        }
+
+        for(int i=0;i<100001;i++){
+            int lb=(i-k>0)?i-k:0;
+            int ub=(i+k<100001)?i+k:100000;
+
+            int x=lower_bound(nums,0,n,lb);
+            int y=upper_bound(nums,0,n,ub);
+            ans=max(ans,min(abs(y-x)-mp[i],numOperations)+mp[i]);
+        }
+        return ans;
+    }
+};
