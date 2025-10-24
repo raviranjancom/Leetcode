452031bfb1301a5ba0cb359610fd6c98cd266995
raviranjancom <erraviranjan030198@gmail.com>
2025-10-24 18:56:30 +0530

    Add 3612-adjacent-increasing-subarrays-detection-i: Accepted - runtime 17ms - memory 40.4MB

---
+++ 3612-adjacent-increasing-subarrays-detection-i/2025-10-24 18.56.30 - Accepted - runtime 17ms - memory 40.4MB.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool check(vector<int>& nums, int a,int b){
+        for(int i=a+1;i<=b;i++){
+            if(nums[i]<=nums[i-1]) return false;
+        }
+        return true;
+    }
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        if(k<=1) return true;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(i+k-1>=n || i+2*k-1>=n) break;
+            if(check(nums,i,i+k-1) && check(nums,i+k,i+2*k-1)) return true;
+        }
+        return false;
+    }
+};
