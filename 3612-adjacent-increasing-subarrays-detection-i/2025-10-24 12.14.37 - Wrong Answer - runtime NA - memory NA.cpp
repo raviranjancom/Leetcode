fdedd4c5ab64e81c05fc3ea5472b41863ee8bb3d
raviranjancom <erraviranjan030198@gmail.com>
2025-10-24 12:14:37 +0530

    Add 3612-adjacent-increasing-subarrays-detection-i: Wrong Answer - runtime NA - memory NA

---
+++ 3612-adjacent-increasing-subarrays-detection-i/2025-10-24 12.14.37 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool check(vector<int>& nums, int k,int x){
+        for(int i=x+1;i<x+k;i++){
+            if(nums[i]<nums[i-1]) return false;
+        }
+        return true;
+    }
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        if(k<=1) return true;
+        int n=nums.size();
+        for(int i=0;i<n-2*k-1;i++){
+            if(check(nums,k,i) && check(nums,k,i+k)) return true;
+        }
+        return false;
+    }
+};
