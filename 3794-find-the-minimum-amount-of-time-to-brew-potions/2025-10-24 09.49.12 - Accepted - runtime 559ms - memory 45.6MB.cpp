34cd673264938e4c41d5916ed33dfb3a8d53a85f
raviranjancom <erraviranjan030198@gmail.com>
2025-10-24 09:49:12 +0530

    Add 3794-find-the-minimum-amount-of-time-to-brew-potions: Accepted - runtime 559ms - memory 45.6MB

---
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/2025-10-24 09.49.12 - Accepted - runtime 559ms - memory 45.6MB.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long minTime(vector<int>& skills, vector<int>& mana) {
+        int n=mana.size(),m=skills.size();
+        vector<long long> prev(m,0),next(m,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(j==0){
+                    next[j]=prev[j]+1LL*skills[j]*mana[i];
+                }
+                else{
+                    next[j]=max(next[j-1],prev[j])+1LL*skills[j]*mana[i];
+                }
+            }
+            // syncronizing time
+            for(int j=m-2;j>=0;j--){
+                next[j]=next[j+1]-1LL*skills[j+1]*mana[i];
+            }
+            prev=next;
+        }
+        return prev[m-1];
+    }
+};
