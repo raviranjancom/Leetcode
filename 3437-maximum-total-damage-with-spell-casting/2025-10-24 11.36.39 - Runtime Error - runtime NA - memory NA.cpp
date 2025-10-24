4f5ddcb4e7b49b80a7829a8ab135739f1e991d4b
raviranjancom <erraviranjan030198@gmail.com>
2025-10-24 11:36:39 +0530

    Add 3437-maximum-total-damage-with-spell-casting: Runtime Error - runtime NA - memory NA

---
+++ 3437-maximum-total-damage-with-spell-casting/2025-10-24 11.36.39 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long solve(vector<int>& power, int i,int n, vector<long long> &dp){
+        if(i>=n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        //skip
+        long long skip=solve(power,i+1,n,dp);
+        // take
+        int i_=upper_bound(begin(power)+i,end(power),power[i]+2)-begin(power);  // next valid index
+
+        long long take=solve(power,i_,n,dp);
+
+        int lb=lower_bound(begin(power)+i,end(power),power[i])-begin(power);
+        int ub=upper_bound(begin(power)+i,end(power),power[i])-begin(power);
+
+        return dp[i]=max(skip,take+(ub-lb)*power[i]);
+    }
+    long long maximumTotalDamage(vector<int>& power) {
+        sort(power.begin(),power.end());
+        int n=power.size();
+        vector<long long> dp(n,-1);
+        return solve(power,0,n,dp);
+    }
+};
