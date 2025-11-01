3c5c93b5d7964a09409464ab3fc7b34e4f9435fe
raviranjancom <erraviranjan030198@gmail.com>
2025-11-01 18:03:16 +0530

    Add 450-delete-node-in-a-bst: Wrong Answer - runtime NA - memory NA

---
+++ 450-delete-node-in-a-bst/2025-11-01 18.03.16 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,89 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==nullptr) return root;
+        if(root->val==key && !root->left && !root->right) return nullptr;
+        TreeNode *ptr=root,*prev=root;
+        if(root->val==key){
+            if(root->left==nullptr){
+                root=root->right;
+                return root;
+            }
+            TreeNode *last=root->left,*prev=root;
+            while(last && last->right){
+                prev=last;
+                last=last->right;
+            }
+            root->val=last->val;
+            prev->right=nullptr;
+            return root;
+        }
+        while(ptr!=nullptr){
+            if(ptr->val==key){
+                TreeNode *last=ptr->right,*next=ptr;
+                while(last && last->left){
+                    next=last;
+                    last=last->left;
+                }
+                if(last==nullptr){
+                    if(prev->left==ptr){
+                        prev->left=ptr->left;
+                        ptr->left=nullptr; ptr->right=nullptr;
+                    }
+                    else{
+                        prev->right=ptr->left;
+                        ptr->left=nullptr; ptr->right=nullptr;
+                    }
+                    return root;
+                }
+                else{
+                    if(prev->left==ptr){
+                        prev->left=last;
+                        last->left=ptr->left;
+                        last->right=ptr->right;
+                        ptr->left=nullptr; ptr->right=nullptr;
+                        if(next==ptr){
+                            last->right=nullptr;
+                        }
+                        else{
+                            next->left=nullptr;
+                        }
+                    }
+                    else{
+                        prev->right=last;
+                        last->left=ptr->left;
+                        last->right=ptr->right;
+                        ptr->left=nullptr; ptr->right=nullptr;
+                        if(next==ptr){
+                            last->right=nullptr;
+                        }
+                        else{
+                            next->left=nullptr;
+                        }
+                    }
+                    return root;
+                }
+            }
+            else if(ptr->val>key){
+                prev=ptr;
+                ptr=ptr->left;
+            }
+            else{
+                prev=ptr;
+                ptr=ptr->right;
+            }
+        }
+        return root;
+    }
+};
