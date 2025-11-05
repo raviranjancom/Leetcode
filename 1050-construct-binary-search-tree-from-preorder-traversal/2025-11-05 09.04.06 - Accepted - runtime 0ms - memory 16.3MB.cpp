2a31123a61a2503e658d5cb53a28d5ff457a9455
raviranjancom <erraviranjan030198@gmail.com>
2025-11-05 09:04:06 +0530

    Add 1050-construct-binary-search-tree-from-preorder-traversal: Accepted - runtime 0ms - memory 16.3MB

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/2025-11-05 09.04.06 - Accepted - runtime 0ms - memory 16.3MB.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *solve(vector<int>& preorder, int& i, int bound){
+        if(i>=preorder.size() || preorder[i]>bound) return nullptr;
+
+        TreeNode *newnode=new TreeNode(preorder[i++]);
+        newnode->left=solve(preorder,i,newnode->val);
+        newnode->right=solve(preorder,i,bound);
+
+        return newnode;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i=0;
+        return solve(preorder,i,INT_MAX);
+    }
+};
