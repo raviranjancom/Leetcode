7966007ad234dc66c49939be232eeebe0fba04a5
raviranjancom <erraviranjan030198@gmail.com>
2025-11-09 10:21:57 +0530

    Add 1732-minimum-one-bit-operations-to-make-integers-zero: Accepted - runtime 0ms - memory 8.1MB

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/2025-11-09 10.21.57 - Accepted - runtime 0ms - memory 8.1MB.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        if(n==0) return 0;
+        int k=0;
+        while(pow(2,k)<=n){
+            k++;
+        }
+        k--;
+        return (pow(2,k+1)-1)-minimumOneBitOperations((int)pow(2,k)^n);
+    }
+};
