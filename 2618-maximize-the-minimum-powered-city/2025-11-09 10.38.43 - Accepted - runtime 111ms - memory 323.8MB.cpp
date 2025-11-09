8a5d94adb3a30b7c3de41cc7b14fc91c85cffec7
raviranjancom <erraviranjan030198@gmail.com>
2025-11-09 10:38:43 +0530

    Add 2618-maximize-the-minimum-powered-city: Accepted - runtime 111ms - memory 323.8MB

---
+++ 2618-maximize-the-minimum-powered-city/2025-11-09 10.38.43 - Accepted - runtime 111ms - memory 323.8MB.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool check(long long mid,vector<long long> diff, int r, int k, int n){
+        vector<long long> temp(diff);
+        long long cumSum=0;
+        for(int i=0;i<n;i++){
+            cumSum+=temp[i];
+            if(cumSum<mid){
+                long long need=mid-cumSum;
+                if(need>k){
+                    return false;
+                }
+                k-=need;
+                cumSum+=need;
+                // i......x......i+2r
+                if(i+2*r+1<n) temp[i+2*r+1]-=need;
+            }
+        }
+        return true;
+    }
+    long long maxPower(vector<int>& stations, int r, int k) {
+        int n=stations.size();
+        vector<long long> diff(n,0);
+        for(int i=0;i<n;i++){
+            diff[max(0,i-r)]+=stations[i];
+            if(i+r+1<n) diff[i+r+1]-=stations[i];
+        }
+        long long left=*min_element(stations.begin(),stations.end());
+        long long right=accumulate(stations.begin(),stations.end(),0LL)+k;
+        long long ans=0;
+        while(left<=right){
+            long long mid=left+(right-left)/2;
+            if(check(mid,diff,r,k,n)){
+                ans=mid;
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+};
