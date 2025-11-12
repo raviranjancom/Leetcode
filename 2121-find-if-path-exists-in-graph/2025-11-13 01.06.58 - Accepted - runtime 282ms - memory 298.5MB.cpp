3a805ba85667fa46dbff1ea9b0eb79743c72306a
raviranjancom <erraviranjan030198@gmail.com>
2025-11-13 01:06:58 +0530

    Add 2121-find-if-path-exists-in-graph: Accepted - runtime 282ms - memory 298.5MB

---
+++ 2121-find-if-path-exists-in-graph/2025-11-13 01.06.58 - Accepted - runtime 282ms - memory 298.5MB.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool dfs(int n,vector<vector<int>>& Adj, int source, int destination,vector<bool> &visited){
+        if(source==destination) return true;
+        visited[source]=true;
+        for(auto &it:Adj[source]){
+            if(!visited[it]){
+                if(dfs(n,Adj,it,destination,visited)) return true;
+            }
+        }
+        return false;
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination){
+        vector<vector<int>> Adj(n);
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            Adj[u].push_back(v);
+            Adj[v].push_back(u);
+        }
+        vector<bool> visited(n,false);
+        return dfs(n,Adj,source,destination,visited);
+    }
+};
