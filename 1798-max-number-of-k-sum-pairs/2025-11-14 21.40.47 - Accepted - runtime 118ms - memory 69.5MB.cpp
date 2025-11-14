efa0080622f0fdc053bdcd3170dd59c272706ad9
raviranjancom <erraviranjan030198@gmail.com>
2025-11-14 21:40:47 +0530

    Add 1798-max-number-of-k-sum-pairs: Accepted - runtime 118ms - memory 69.5MB

---
+++ 1798-max-number-of-k-sum-pairs/2025-11-14 21.40.47 - Accepted - runtime 118ms - memory 69.5MB.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(mp.count(k-nums[i])>0){
+                if(mp[k-nums[i]]>0){
+                    mp[k-nums[i]]--;
+                    ans++;
+                }
+                else{
+                    mp[nums[i]]++;
+                }
+            }
+            else{
+                mp[nums[i]]++;
+            }
+        }
+        return ans;
+    }
+};
