104b09b8479ba1929799798205697895e9a7bc8a
raviranjancom <erraviranjan030198@gmail.com>
2025-11-15 10:09:39 +0530

    Add 26-remove-duplicates-from-sorted-array: Accepted - runtime 0ms - memory 22.7MB

---
+++ 26-remove-duplicates-from-sorted-array/2025-11-15 10.09.39 - Accepted - runtime 0ms - memory 22.7MB.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums){
+        int n=nums.size();
+        int k=1;
+        for(int i=1;i<n;i++){
+            if(nums[k-1]!=nums[i]){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
