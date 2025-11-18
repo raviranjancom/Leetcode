dd10f3c41c9652e08b93863fae7d39b247ed56ad
raviranjancom <erraviranjan030198@gmail.com>
2025-11-18 23:11:55 +0530

    Add 717-1-bit-and-2-bit-characters: Accepted - runtime 0ms - memory 13.1MB

---
+++ 717-1-bit-and-2-bit-characters/2025-11-18 23.11.55 - Accepted - runtime 0ms - memory 13.1MB.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n=bits.size();
+        int i=0;
+        while(i<n){
+            if(i==n-1) return true;
+            else if(bits[i]==1){
+                i+=2;
+            }
+            else i++;
+        }
+        return false;
+    }
+};
