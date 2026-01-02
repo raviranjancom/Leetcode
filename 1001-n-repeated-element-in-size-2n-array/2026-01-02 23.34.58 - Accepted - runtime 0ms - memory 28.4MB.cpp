3251db7574f27aee7e71f2e9a80fa099fc55f495
raviranjancom <erraviranjan030198@gmail.com>
2026-01-02 23:34:58 +0530

    Add 1001-n-repeated-element-in-size-2n-array: Accepted - runtime 0ms - memory 28.4MB

---
+++ 1001-n-repeated-element-in-size-2n-array/2026-01-02 23.34.58 - Accepted - runtime 0ms - memory 28.4MB.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& arr){
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(i+2<n && arr[i]==arr[i+2]){
+                return arr[i];
+            }
+            if(i+1<n && arr[i]==arr[i+1]){
+                return arr[i];
+            }
+            if(i+3<n && arr[i]==arr[i+3]){
+                return arr[i];
+            }
+        }
+        return 0;
+    }
+};
