1c9b10c7c3f868b847079c55c0bd9fb5dc8f3aa2
raviranjancom <erraviranjan030198@gmail.com>
2026-01-05 11:33:21 +0530

    Add 2465-shifting-letters-ii: Wrong Answer - runtime NA - memory NA

---
+++ 2465-shifting-letters-ii/2026-01-05 11.33.21 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    string shiftingLetters(string s, vector<vector<int>>& shifts){
+        int n=s.length();
+        vector<int> val(n,0);
+        for(int i=0;i<shifts.size();i++){
+            int start=shifts[i][0];
+            int end=shifts[i][1];
+            int dir=shifts[i][2];
+
+            if(dir==1){
+                val[start]+=1;
+                if(end+1<n) val[end+1]+=-1;
+            }
+            else{
+                val[start]-=1;
+                if(end+1<n) val[end+1]+=1;
+            }
+        }
+        for(int i=1;i<n;i++){
+            val[i]+=val[i-1];
+        }
+        for(int i=0;i<n;i++){
+            s[i]=((s[i]-'a'+val[i])%26)+'a';
+        }
+        return s;
+    }
+};
