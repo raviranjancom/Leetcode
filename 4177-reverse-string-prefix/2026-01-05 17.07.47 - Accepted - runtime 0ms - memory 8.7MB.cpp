b664d4e31f5e40fda4620b4113dcecf5bdab4341
raviranjancom <erraviranjan030198@gmail.com>
2026-01-05 17:07:47 +0530

    Add 4177-reverse-string-prefix: Accepted - runtime 0ms - memory 8.7MB

---
+++ 4177-reverse-string-prefix/2026-01-05 17.07.47 - Accepted - runtime 0ms - memory 8.7MB.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string reversePrefix(string s, int k){
+        int i=0,j=k-1;
+        while(i<=j){
+            swap(s[i],s[j]);
+            i++; j--;
+        }
+        return s;
+    }
+};
