9593e99877934a7064011bef1f5e5869ecff9b1e
raviranjancom <erraviranjan030198@gmail.com>
2026-01-05 17:37:55 +0530

    Add 4152-minimum-subarray-length-with-distinct-sum-at-least-k: Wrong Answer - runtime NA - memory NA

---
+++ 4152-minimum-subarray-length-with-distinct-sum-at-least-k/2026-01-05 17.37.55 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minLength(vector<int>& nums, int k){
+        int ans=1e9;
+        int n=nums.size();
+        int i=0,j=0;
+        int sum=0;
+        unordered_map<int,int> mp;
+        while(i<n && j<n){
+            if(sum<k){
+                int size=mp.size();
+                mp[nums[j]]++;
+                if(size<mp.size()){
+                    sum+=nums[j];
+                }
+                j++;
+            }
+            else{
+                ans=min(ans,j-i);
+                mp[nums[i]]--;
+                int size=mp.size();
+                if(mp[nums[i]]==0){
+                    mp.erase(nums[i]);
+                }
+                
+                if(size>mp.size()){
+                    sum-=nums[i];
+                }
+                i++;
+            }
+        }
+        return (ans==1e9)?-1:ans;
+    }
+};
