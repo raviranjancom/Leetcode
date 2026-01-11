bd0d2d70878ca4187a3034ca04025c615d4c1a6a
raviranjancom <erraviranjan030198@gmail.com>
2026-01-11 21:20:04 +0530

    Add 1502-construct-k-palindrome-strings: Wrong Answer - runtime NA - memory NA

---
+++ 1502-construct-k-palindrome-strings/2026-01-11 21.20.04 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k){
+        if(k==s.length()) return true;
+        unordered_map<char,int> mp;
+        for(auto &it:s){
+            mp[it]++;
+        }
+        int nonPairCount=0;
+        int pairCount=0;
+        for(auto &it:mp){
+            nonPairCount+=it.second%2;
+            pairCount+=it.second/2;
+        }
+        if(nonPairCount>k) return false;
+        if(pairCount<k) return false;
+        return true;
+    }
+};
