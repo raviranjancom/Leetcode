0547382192516a6f2f0b2f55ef0b5b970fb5b861
raviranjancom <erraviranjan030198@gmail.com>
2026-01-11 08:14:48 +0530

    Add 4129-number-of-centered-subarrays: Accepted - runtime 211ms - memory 86.5MB

---
+++ 4129-number-of-centered-subarrays/2026-01-11 08.14.48 - Accepted - runtime 211ms - memory 86.5MB.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int centeredSubarrays(vector<int>& nums){
+        int n=nums.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            unordered_map<int,int> mp;
+            int sum=0;
+            for(int j=i;j<n;j++){
+                mp[nums[j]]++;
+                sum+=nums[j];
+                if(mp.find(sum)!=mp.end()) ans++;
+            }
+        }
+        return ans;
+    }
+};
