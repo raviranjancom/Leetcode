9b947e63365de02576bf240c73aebe86f540b824
raviranjancom <erraviranjan030198@gmail.com>
2026-01-15 12:31:44 +0530

    Add 3864-count-the-number-of-computer-unlocking-permutations: Accepted - runtime 0ms - memory 92.8MB

---
+++ 3864-count-the-number-of-computer-unlocking-permutations/2026-01-15 12.31.44 - Accepted - runtime 0ms - memory 92.8MB.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    int fact(int n){
+        long long ans=1;
+        for(int i=2;i<=n;i++){
+            ans=(ans*i)%mod;
+        }
+        return ans;
+    }
+    int countPermutations(vector<int>& complexity){
+        int ans=0;
+        int n=complexity.size();
+        for(int i=1;i<n;i++){
+            if(complexity[0]>=complexity[i]){
+                return 0;
+            }
+        }
+        return fact(n-1);
+    }
+};
