9381882aaa33aba2ef7a0f1e8aee59c0d47b08a1
raviranjancom <erraviranjan030198@gmail.com>
2026-01-18 08:52:23 +0530

    Add 4067-design-auction-system: Wrong Answer - runtime NA - memory NA

---
+++ 4067-design-auction-system/2026-01-18 08.52.23 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,41 @@
+class AuctionSystem {
+public:
+    map<int,map<int,int>> mp;
+    map<int,set<pair<int,int>>> st;
+    AuctionSystem(){
+        
+    }
+    
+    void addBid(int userId, int itemId, int bidAmount){
+        mp[itemId][userId]=bidAmount;
+        st[itemId].insert({bidAmount,userId});
+    }
+    
+    void updateBid(int userId, int itemId, int newAmount){
+        int prevAmount=mp[itemId][userId];
+        mp[itemId][userId]=newAmount;
+        st[itemId].erase({prevAmount,userId});
+        st[itemId].insert({newAmount,userId});
+    }
+    
+    void removeBid(int userId, int itemId){
+        int prevAmount=mp[itemId][userId];
+        mp[itemId].erase(userId);
+        st[itemId].erase({prevAmount,userId});
+    }
+    
+    int getHighestBidder(int itemId){
+        auto it=st[itemId];
+        if(it.size()==0) return -1;
+        return it.rbegin()->second;
+    }
+};
+
+/**
+ * Your AuctionSystem object will be instantiated and called as such:
+ * AuctionSystem* obj = new AuctionSystem();
+ * obj->addBid(userId,itemId,bidAmount);
+ * obj->updateBid(userId,itemId,newAmount);
+ * obj->removeBid(userId,itemId);
+ * int param_4 = obj->getHighestBidder(itemId);
+ */
