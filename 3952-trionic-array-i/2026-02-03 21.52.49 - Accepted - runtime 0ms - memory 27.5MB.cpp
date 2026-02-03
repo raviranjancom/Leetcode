bb3aa63834947a84c3fcd05b5c8e83d366d609d7
raviranjancom <erraviranjan030198@gmail.com>
2026-02-03 21:52:49 +0530

    Add 3952-trionic-array-i: Accepted - runtime 0ms - memory 27.5MB

---
+++ 3952-trionic-array-i/2026-02-03 21.52.49 - Accepted - runtime 0ms - memory 27.5MB.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums){
+        int n=nums.size();
+        int i=0;
+        for(i=0;i<n-1;i++){
+            if(nums[i]<nums[i+1]) continue;
+            else break;
+        }
+        int p=i;
+        if(i==n-1 || i==0) return false;
+        for(i=i;i<n-1;i++){
+            if(nums[i]>nums[i+1]) continue;
+            else break;
+        }
+        if(i==n-1 || i==p) return false;
+        for(i=i;i<n-1;i++){
+            if(nums[i]<nums[i+1]) continue;
+            else return false;
+        }
+        return true;
+    }
+};
