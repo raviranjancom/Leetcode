de3abc0117a3c38aae395df63f60dc14a0616524
raviranjancom <erraviranjan030198@gmail.com>
2026-02-04 22:19:34 +0530

    Add 3956-trionic-array-ii: Time Limit Exceeded - runtime NA - memory NA

---
+++ 3956-trionic-array-ii/2026-02-04 22.19.34 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int n;
+    long long solve(int idx,int trend, vector<int> &nums){
+        if(idx>=n){
+            if(trend==3){
+                return 0;
+            }
+            else{
+                return LLONG_MIN/2;
+            }
+        }
+        long long take=LLONG_MIN/2;
+        long long skip=LLONG_MIN/2;
+        // skipping for next index to start in future
+        if(trend==0){
+            skip=solve(idx+1,0,nums);
+        }
+        // I am at trend 3 and i can now end at idx
+        if(trend==3){
+            take=nums[idx]; // finish it here
+        }
+        if(idx+1<n){
+            int curr=nums[idx];
+            int next=nums[idx+1];
+            if(trend==0 && next>curr){
+                take=max(take,curr+solve(idx+1,1,nums));
+            }
+            else if(trend==1){
+                if(next>curr){
+                    take=max(take,curr+solve(idx+1,1,nums));
+                }
+                else if(next<curr){
+                    take=max(take,curr+solve(idx+1,2,nums));
+                }
+            }
+            else if(trend==2){
+                if(next<curr){
+                    take=max(take,curr+solve(idx+1,2,nums));
+                }
+                else if(next>curr){
+                    take=max(take,curr+solve(idx+1,3,nums));
+                }
+            }
+            else if(trend==3 && next>curr){
+                take=max(take,curr+solve(idx+1,3,nums));
+            }
+        }
+        return max(take,skip);
+    }
+    long long maxSumTrionic(vector<int>& nums){
+        n=nums.size();
+        dp.assign(n,vector<int>(4,-1));
+        return solve(0,0,nums);
+    }
+};
