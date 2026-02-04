47a611f4d891cffcf0ad5f87843f2a870987c0cc
raviranjancom <erraviranjan030198@gmail.com>
2026-02-04 16:56:45 +0530

    Add 55-jump-game: Accepted - runtime 0ms - memory 52.3MB

---
+++ 55-jump-game/2026-02-04 16.56.45 - Accepted - runtime 0ms - memory 52.3MB.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums){
+        int n=nums.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=max(ans,i+nums[i]);
+            if(ans<=i && i!=n-1) return false;
+        }
+        return true;
+    }
+};
