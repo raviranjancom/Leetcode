d1598534edd1fd0aae487282d3e2fbc35662632c
raviranjancom <erraviranjan030198@gmail.com>
2026-02-05 13:56:29 +0530

    Add 3651-transformed-array: Runtime Error - runtime NA - memory NA

---
+++ 3651-transformed-array/2026-02-05 13.56.29 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums){
+        int n=nums.size();
+        vector<int> ans(n);
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                ans[i]=nums[(i+nums[i])%n];
+            }
+            else if(nums[i]<0){
+                ans[i]=nums[(i+nums[i]+n)%n];
+            }
+            else{
+                ans[i]=nums[i];
+            }
+        }
+        return ans;
+    }
+};
