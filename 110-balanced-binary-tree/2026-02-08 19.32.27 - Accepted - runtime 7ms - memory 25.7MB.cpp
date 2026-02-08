4ca731f75b5cd2885ba1a81c0a2574dce4468524
raviranjancom <erraviranjan030198@gmail.com>
2026-02-08 19:32:27 +0530

    Add 110-balanced-binary-tree: Accepted - runtime 7ms - memory 25.7MB

---
+++ 110-balanced-binary-tree/2026-02-08 19.32.27 - Accepted - runtime 7ms - memory 25.7MB.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<TreeNode *,int> dp;
+    int height(TreeNode *r){
+        if(r==nullptr) return 0;
+        if(dp.count(r) && dp[r]!=-1) return dp[r];
+        return dp[r]=max(height(r->left),height(r->right))+1;
+    }
+    int H(TreeNode *r){
+        return (r==nullptr)?0:dp[r];
+    }
+    bool isBalanced(TreeNode* root){
+        height(root);
+        queue<TreeNode *> q;
+        if(root) q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                TreeNode *x=q.front(); q.pop();
+                if(x->left) q.push(x->left);
+                if(x->right) q.push(x->right);
+                if(abs(H(x->right)-H(x->left))>1) return false;
+            }
+        }
+        return true;
+    }
+};
