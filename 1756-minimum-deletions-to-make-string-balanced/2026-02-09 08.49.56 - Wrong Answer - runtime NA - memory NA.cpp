fe1b558a3824082743ee30eaafa692647b13bad4
raviranjancom <erraviranjan030198@gmail.com>
2026-02-09 08:49:56 +0530

    Add 1756-minimum-deletions-to-make-string-balanced: Wrong Answer - runtime NA - memory NA

---
+++ 1756-minimum-deletions-to-make-string-balanced/2026-02-09 08.49.56 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumDeletions(string s){
+        int n=s.length();
+        int countA=0,countB=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='a') countA++;
+            if(s[i]=='b') countB++;
+        }
+        if(countA==0 || countB==0) return 0;
+        int x=0,y=0,ans=1e9;
+        for(int i=0;i<n;i++){
+            if(s[i]=='a') x++;
+            if(s[i]=='b') y++;
+            ans=min(ans,y+countA-x);
+        }
+        return ans;
+    }
+};
