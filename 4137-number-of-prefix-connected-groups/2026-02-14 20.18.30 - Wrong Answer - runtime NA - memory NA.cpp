4406dad7041752a63b084e1c454a8884c276ef92
raviranjancom <erraviranjan030198@gmail.com>
2026-02-14 20:18:30 +0530

    Add 4137-number-of-prefix-connected-groups: Wrong Answer - runtime NA - memory NA

---
+++ 4137-number-of-prefix-connected-groups/2026-02-14 20.18.30 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int prefixConnected(vector<string>& words, int k){
+        unordered_map<string,int> mp;
+        int n=words.size();
+        for(int i=0;i<n;i++){
+            if(words.size()>=k){
+                auto str=words[i].substr(0,k);
+                mp[str]++;
+            }
+        }
+        int ans=0;
+        for(auto &it:mp){
+            if(it.second>1) ans++;
+        }
+        return ans;
+    }
+};
