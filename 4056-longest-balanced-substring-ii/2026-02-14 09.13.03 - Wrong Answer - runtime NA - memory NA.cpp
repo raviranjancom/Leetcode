4eea1e40192f166f439d045f0ddd14d226d4d1d5
raviranjancom <erraviranjan030198@gmail.com>
2026-02-14 09:13:03 +0530

    Add 4056-longest-balanced-substring-ii: Wrong Answer - runtime NA - memory NA

---
+++ 4056-longest-balanced-substring-ii/2026-02-14 09.13.03 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestBalanced(string s){
+        int n=s.length();
+        int ans=0;
+        map<vector<int>,int> mp;
+        mp[{0,0,0}]=-1;
+        int a=0,b=0,c=0;
+        for(int i=0;i<n;i++){
+            a+=(s[i]=='a')?1:0;
+            b+=(s[i]=='b')?1:0;
+            c+=(s[i]=='c')?1:0;
+
+            int x=(a==0)?INT_MAX:a;
+            int y=(b==0)?INT_MAX:b;
+            int z=(c==0)?INT_MAX:c;
+            int mini=min(x,min(y,z));
+
+            int p=(a==0)?0:a-mini;
+            int q=(b==0)?0:b-mini;
+            int r=(c==0)?0:c-mini;
+            if(mp.find({p,q,r})!=mp.end()){
+                auto it=mp[{p,q,r}];
+                ans=max(ans,i-it);
+            }
+            if(mp.find({a,b,c})==mp.end()) mp[{a,b,c}]=i;
+        }
+        return ans;
+    }
+};
+// 0, 0, 0
+// 1, 0, 0
+// 1, 1, 0
+// 1, 2, 0
+// 2, 2, 0
+// 2, 2, 1
