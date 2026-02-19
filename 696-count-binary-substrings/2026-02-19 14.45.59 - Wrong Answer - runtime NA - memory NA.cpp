8e4ffde280f958990b323062a7675c46d3202af3
raviranjancom <erraviranjan030198@gmail.com>
2026-02-19 14:45:59 +0530

    Add 696-count-binary-substrings: Wrong Answer - runtime NA - memory NA

---
+++ 696-count-binary-substrings/2026-02-19 14.45.59 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s){
+        int n=s.length();
+        int zeros=0,ones=0;
+        if(s[0]=='0') zeros++;
+        if(s[0]=='1') ones++;
+        int ans=0;
+        for(int i=1;i<n;i++){
+            if(s[i]=='1'){
+                if(s[i-1]=='0'){
+                    ans+=(ones>=zeros)?zeros:0;
+                    ones=1;
+                }
+                else{
+                    ones++;
+                }
+            }
+            else{
+                if(s[i-1]=='1'){
+                    ans+=(zeros>=ones)?ones:0;
+                    zeros=1;
+                }
+                else{
+                    zeros++;
+                }
+            }
+        }
+        ans+=min(zeros,ones);
+        return ans;
+    }
+};
