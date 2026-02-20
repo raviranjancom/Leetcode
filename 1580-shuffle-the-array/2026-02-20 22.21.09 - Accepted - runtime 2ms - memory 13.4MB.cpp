0cbc6f509e1b337b239153f6e81947cde8ba2e61
raviranjancom <erraviranjan030198@gmail.com>
2026-02-20 22:21:09 +0530

    Add 1580-shuffle-the-array: Accepted - runtime 2ms - memory 13.4MB

---
+++ 1580-shuffle-the-array/2026-02-20 22.21.09 - Accepted - runtime 2ms - memory 13.4MB.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n){
+        int i=0,j=n;
+        vector<int> ans;
+        while(i<n && j<2*n){
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+            i+=1; j+=1;
+        }
+        return ans;
+    }
+};
